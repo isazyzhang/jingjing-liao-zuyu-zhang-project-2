abfd4e826515a79e64a18b32c5d3722f6a62b7c0
isazyzhang <33339687+isazyzhang@users.noreply.github.com>
2021-11-19 23:59:07 -0800

    fixed bugs

--- src/NormalGame.jsx
+++ src/NormalGame.jsx
@@ -1,10 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-=======
-import "./css/NormalGame.css";
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
->>>>>>> df795d8f98a6355c5e784d028e3d0e09516f184b
 import PlayerBoard from "./PlayerBoard";
 import "./css/Boards.css";
 import OpponentBoard from "./OpponentBoard";
@@ -14,11 +8,7 @@ import "./css/NormalGame.css";
 import { useState } from "react";
 
 export default function NormalGame() {
-  const humanClickCount = useSelector((state) => state.AIGame.count);
-  const computerClickCount = useSelector((state) => state.playerGame.count);
-  const turn = useSelector((state) => state.turn);
 
-<<<<<<< HEAD
     const humanClickCount = useSelector((state) => state.AIGame.count);
     const computerClickCount = useSelector((state) => state.playerGame.count);
     const [modeState, setModeState] = useState(0);
@@ -65,87 +55,3 @@ export default function NormalGame() {
         </div>
     );
 }
-=======
-  const totalScore = 17;
-  let winner = "";
-  if (humanClickCount === totalScore) {
-    winner = "Congratulations, You Won!";
-  } else if (computerClickCount === totalScore) {
-    winner = "Game Over! The Computer Won!";
-  }
-
-  return (
-    <div>
-      <ResetButton text="Reset The Game" />
-      {/* <div class="count">
-                Score: {turn === 0 ? humanClickCount : computerClickCount}
-            </div> */}
-      {/* <div class="count">{turn === 0 ? "Human's Turn" : "Computer's Turn"}</div> */}
-      <div class="winner">{winner} </div>
-      <div id="boards">
-        <div id="opponent">
-          <h1>Opponent Board</h1>
-          <div>
-            <OpponentBoard />
-          </div>
-        </div>
-
-        <div id="own">
-          <h1>Your Board</h1>
-          <div>
-            <PlayerBoard />
-          </div>
-        </div>
-      </div>
-      {/* {turn === 0 ? <ComputerBoard /> : <HumanBoard />} */}
-    </div>
-  );
-
-  // return (
-  //     <div>
-  //       <div>
-  //         <ResetButton text="Reset The Game" />
-  //       </div>
-  //       <div class="count">
-  //         Score: {turn === 0 ? humanClickCount : computerClickCount}
-  //       </div>
-  //       <div id="boards">
-  //         <div id="opponent">
-  //             <h1>Opponent Board</h1>
-  //               <div>
-  //                 <ComputerBoard />
-  //               </div>
-  //         </div>
-
-  //         <div id="own">
-  //             <h1>Your Board</h1>
-  //               <div>
-  //                 <HumanBoard />
-  //               </div>
-  //         </div>
-  //       </div>
-  //       </div>
-  //   )
-
-  // return (
-  //     <div>
-  //         <div id="boards">
-  //             <div id="opponent" turn={turn}>
-  //                 <h1>Opponent Board</h1>
-  //                 <div>
-  //                     <OpponentBoard />
-  //                 </div>
-  //             </div>
-  //             <div id="own" turn={turn}>
-  //                 <h1>
-  //                     Your Board
-  //                 </h1>
-  //                 <div>
-  //                     <PlayerBoard />
-  //                 </div>
-  //             </div>
-  //         </div>
-  //     </div>
-  // )
-}
->>>>>>> df795d8f98a6355c5e784d028e3d0e09516f184b
--- src/OpponentTile.jsx
+++ src/OpponentTile.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { useDispatch } from "react-redux";
 
 export default function OpponentTile(props) {
-<<<<<<< HEAD
     const condition = props.condition;
     const dispatch = useDispatch();
     return (
@@ -25,28 +24,3 @@ export default function OpponentTile(props) {
         </div>
     )
 }
-=======
-  const condition = props.condition; // n or s or a
-  const dispatch = useDispatch();
-  return (
-    <div
-      onClick={() => {
-        dispatch({
-          type: "HumanClickSquare",
-          x: props.x,
-          y: props.y,
-        });
-        setTimeout(() => {
-          dispatch({
-            type: "HumanChangeTurn",
-          });
-        }, 2000);
-      }}
-      id="opponentTile"
-      class={condition}
-    >
-      {condition}
-    </div>
-  );
-}
->>>>>>> df795d8f98a6355c5e784d028e3d0e09516f184b
